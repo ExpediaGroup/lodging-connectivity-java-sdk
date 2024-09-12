1f4fabe15da063aa655b3bee116cc05b00cb5c22
Mohammad Dwairi <49045447+Mohammad-Dwairi@users.noreply.github.com>
2024-09-12 16:11:59 +0300

    feat: Upgrade to Apollo Kotlin V4 (#32)

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/adapter/ZoneDateTimeAdapter.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/adapter/ZoneDateTimeAdapter.kt
@@ -16,10 +16,10 @@
 
 package com.expediagroup.sdk.lodgingconnectivity.graphql.adapter
 
-import com.apollographql.apollo3.api.Adapter
-import com.apollographql.apollo3.api.CustomScalarAdapters
-import com.apollographql.apollo3.api.json.JsonReader
-import com.apollographql.apollo3.api.json.JsonWriter
+import com.apollographql.apollo.api.Adapter
+import com.apollographql.apollo.api.CustomScalarAdapters
+import com.apollographql.apollo.api.json.JsonReader
+import com.apollographql.apollo.api.json.JsonWriter
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
