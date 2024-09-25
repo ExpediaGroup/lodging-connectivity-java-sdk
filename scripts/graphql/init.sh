be095505adb0379e52c138e56b1e26d6d1bdea05
mohnoor94 <moh.noor94@gmail.com>
2024-09-25 17:38:29 +0300

    chore: update submodule

--- scripts/graphql/init.sh
+++ scripts/graphql/init.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Print commands and their arguments as they are executed
+
+echo "Initializing submodules..."
 git submodule init
+
+echo "Updating submodules..."
 git submodule update --remote --merge
 
+echo "Changing directory to .git/modules/code/src/main/graphql..."
 cd .git/modules/code/src/main/graphql || exit
 
-mkdir info
+echo "Creating info directory..."
+mkdir -p info
 cd info || exit
 
+echo "Configuring sparse checkout..."
 cat > sparse-checkout <<EOL
 /*
 !CHANGELOG.md
@@ -20,6 +29,11 @@ EOL
 
 git config core.sparseCheckout true
 
+echo "Returning to the top-level directory..."
 cd "$(git rev-parse --show-toplevel)" || exit
+
+echo "Reading tree..."
 git read-tree -mu HEAD
+
+echo "Pulling latest changes..."
 git pull origin "$(git rev-parse --abbrev-ref HEAD)"
