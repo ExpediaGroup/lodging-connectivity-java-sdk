0234c1059dd9728e07be1cf8506a98cb54a51a82
mdwairi <mdwairy@gmail.com>
2024-10-15 15:45:01 +0300

    feat: add new ReservationClient

---
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/supply/ReservationClient.kt
@@ -0,0 +1,38 @@
+package com.expediagroup.sdk.lodgingconnectivity.graphql.supply
+
+import com.expediagroup.sdk.lodgingconnectivity.configuration.ClientConfiguration
+import com.expediagroup.sdk.lodgingconnectivity.configuration.EndpointProvider
+import com.expediagroup.sdk.lodgingconnectivity.graphql.BaseGraphQLClient
+import com.expediagroup.sdk.lodgingconnectivity.graphql.GraphQLExecutor
+
+/**
+ * A client for interacting with EG Lodging Connectivity Reservations GraphQL API
+ *
+ * This client is configured with a `ClientConfiguration` that includes authentication details,
+ * and it automatically determines the appropriate API endpoints based on the environment (e.g., production or test).
+ *
+ * In addition, this client can be configured to target the sandbox environment by passing `ClientEnvironment.SANDBOX_PROD` or
+ * `ClientEnvironment.SANDBOX_TEST` to the `environment` configuration option.
+ *
+ * @constructor Creates a new instance of `ReservationClient` using the provided configuration.
+ * @param config The `ClientConfiguration` that includes API credentials and other optional parameters such as environment,
+ * timeouts, and logging masking options.
+ *
+ * Example usage:
+ * ```
+ * ReservationClient(
+ *     ClientConfiguration
+ *         .builder()
+ *         .key("API_KEY")
+ *         .secret("API_SECRET")
+ *         .build()
+ * )
+ * ```
+ */
+class ReservationClient(config: ClientConfiguration) :
+    GraphQLExecutor by BaseGraphQLClient(
+        config.toExpediaGroupClientConfiguration(
+            endpointProvider = EndpointProvider::getSandboxDataManagementClientEndpoint,
+            authEndpointProvider = EndpointProvider::getAuthEndpoint
+        )
+    )
