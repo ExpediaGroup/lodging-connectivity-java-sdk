ea70221e8e938430cf986942f793729202a8e9c2
mdwairi <mdwairy@gmail.com>
2024-10-28 20:18:43 +0300

    fix: minor fixes

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/extension/ApolloErrorExtension.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/extension/ApolloErrorExtension.kt
@@ -3,9 +3,8 @@ package com.expediagroup.sdk.lodgingconnectivity.graphql.extension
 import com.apollographql.apollo.api.Error
 import com.expediagroup.sdk.lodgingconnectivity.graphql.model.response.RawResponseError
 
-fun Error.toRawResponseError(): RawResponseError {
-    return RawResponseError(
+fun Error.toRawResponseError() =
+    RawResponseError(
         message = this.message,
         path = this.path?.map { it.toString() }
     )
-}
