d42038e0fbf6366d4b2fa613dd7441ba8da8f9e8
mdwairi <mdwairy@gmail.com>
2024-10-28 20:18:43 +0300

    fix: minor fixes

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/model/paging/PaginationControl.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/model/paging/PaginationControl.kt
@@ -1,6 +1,6 @@
 package com.expediagroup.sdk.lodgingconnectivity.graphql.model.paging
 
-data class PaginationControl(
+data class PaginationControl @JvmOverloads constructor(
     val pageSize: Int? = null,
     val cursor: String? = null
 )
