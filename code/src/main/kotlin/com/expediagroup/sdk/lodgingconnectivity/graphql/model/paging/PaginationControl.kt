082894c984c6e176e1d437db596eaecfe0dd90f5
mdwairi <mdwairy@gmail.com>
2024-10-29 16:35:07 +0300

    fix: simplify paginator setup

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/model/paging/PaginationControl.kt
+++
@@ -1,6 +0,0 @@
-package com.expediagroup.sdk.lodgingconnectivity.graphql.model.paging
-
-data class PaginationControl @JvmOverloads constructor(
-    val pageSize: Int? = null,
-    val cursor: String? = null
-)
--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/supply/reservation/function/GetPropertyReservationsSummaryFun.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/supply/reservation/function/GetPropertyReservationsSummaryFun.kt
@@ -4,9 +4,8 @@ import com.expediagroup.sdk.core.model.exception.service.ExpediaGroupServiceExce
 import com.expediagroup.sdk.lodgingconnectivity.graphql.GraphQLExecutor
 import com.expediagroup.sdk.lodgingconnectivity.graphql.extension.orNullIfBlank
 import com.expediagroup.sdk.lodgingconnectivity.graphql.model.paging.PageInfo
-import com.expediagroup.sdk.lodgingconnectivity.graphql.model.paging.PaginationControl
+import com.expediagroup.sdk.lodgingconnectivity.graphql.model.response.PaginatedResponse
 import com.expediagroup.sdk.lodgingconnectivity.graphql.model.response.RawResponse
-import com.expediagroup.sdk.lodgingconnectivity.graphql.model.response.Response
 import com.expediagroup.sdk.lodgingconnectivity.graphql.supply.PropertyReservationsSummaryQuery
 import com.expediagroup.sdk.lodgingconnectivity.graphql.supply.fragment.ReservationSummaryData
 import com.expediagroup.sdk.lodgingconnectivity.graphql.supply.reservation.constant.Constant
@@ -17,24 +16,22 @@ import kotlin.jvm.optionals.getOrNull
 data class ReservationsSummaryResponse(
     override val data: List<Optional<ReservationSummaryData>>,
     override val rawResponse: RawResponse<PropertyReservationsSummaryQuery.Data>,
-    val currentPageInfo: PageInfo,
-    val nextPagePaginationControl: PaginationControl? = null
-) : Response<List<Optional<ReservationSummaryData>>, PropertyReservationsSummaryQuery.Data>
+    override val pageInfo: PageInfo,
+) : PaginatedResponse<List<Optional<ReservationSummaryData>>, PropertyReservationsSummaryQuery.Data>
 
 @JvmOverloads
 fun getPropertyReservationsSummaryFun(
     client: GraphQLExecutor,
     input: PropertyReservationsInput,
-    paginationControl: PaginationControl? = null,
+    cursor: String? = null,
+    pageSize: Int? = Constant.RESERVATIONS_DEFAULT_PAGE_SIZE
 ): ReservationsSummaryResponse {
-    val pageSize: Int = paginationControl?.pageSize ?: Constant.RESERVATIONS_DEFAULT_PAGE_SIZE
-
     val operation = PropertyReservationsSummaryQuery
         .builder()
         .propertyId(input.propertyId)
         .idSource(input.idSource.orElse(Optional.empty()))
         .pageSize(pageSize)
-        .cursor(Optional.ofNullable(paginationControl?.cursor))
+        .cursor(Optional.ofNullable(cursor))
         .filter(input.filter.orElse(Optional.empty()))
         .checkOutDate(input.checkOutDate.orElse(Optional.empty()))
         .build()
@@ -50,23 +47,16 @@ fun getPropertyReservationsSummaryFun(
     val nextPageInfo = reservationsPage.pageInfo.getOrNull()
 
     val currentPageInfo = PageInfo(
-        cursor = paginationControl?.cursor,
+        cursor = cursor,
+        nextPageCursor = nextPageInfo?.endCursor?.orNullIfBlank(),
         hasNext = nextPageInfo?.hasNextPage ?: false,
         pageSize = reservationsPage.edges.size,
         totalCount = reservationsPage.totalCount.getOrNull()
     )
 
-    val nextPagePaginationControl = if (nextPageInfo != null) {
-        PaginationControl(
-            pageSize = pageSize,
-            cursor = nextPageInfo.endCursor.orNullIfBlank()
-        )
-    } else null
-
     return ReservationsSummaryResponse(
         data = reservationsPage.edges.map { edgeOptional -> edgeOptional.map { it.node.reservationSummaryData } },
         rawResponse = response,
-        currentPageInfo = currentPageInfo,
-        nextPagePaginationControl = nextPagePaginationControl,
+        pageInfo = currentPageInfo
     )
 }
