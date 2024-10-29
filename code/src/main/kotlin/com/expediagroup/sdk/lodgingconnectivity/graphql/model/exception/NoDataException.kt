4915553d21c92fedda9f557f003d117360760610
mdwairi <mdwairy@gmail.com>
2024-10-29 16:41:35 +0300

    fix: replace RuntimeException with ExpediaGroupServiceException

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/model/exception/NoDataException.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/graphql/model/exception/NoDataException.kt
@@ -1,9 +1,10 @@
 package com.expediagroup.sdk.lodgingconnectivity.graphql.model.exception
 
+import com.expediagroup.sdk.core.model.exception.service.ExpediaGroupServiceException
 import com.expediagroup.sdk.lodgingconnectivity.graphql.model.response.RawResponseError
 
 class NoDataException(
     message: String? = null,
     cause: Throwable? = null,
     val errors: List<RawResponseError>,
-) : RuntimeException(message, cause)
+) : ExpediaGroupServiceException(message, cause)
