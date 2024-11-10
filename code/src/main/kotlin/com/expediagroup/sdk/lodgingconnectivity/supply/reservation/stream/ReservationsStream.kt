5567db399ae99e35915e92ec0d5234559aaebad6
mdwairi <mdwairy@gmail.com>
2024-11-11 01:00:04 +0300

    fix: rename PropertyReservationsPaginator and PropertyReservationStream

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/supply/reservation/stream/ReservationsStream.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/supply/reservation/stream/ReservationsStream.kt
@@ -2,12 +2,9 @@ package com.expediagroup.sdk.lodgingconnectivity.supply.reservation.stream
 
 import com.expediagroup.sdk.graphql.model.paging.PaginatedStream
 import com.expediagroup.sdk.lodgingconnectivity.supply.operation.fragment.ReservationData
-import com.expediagroup.sdk.lodgingconnectivity.supply.reservation.paginator.PropertyReservationsPaginator
+import com.expediagroup.sdk.lodgingconnectivity.supply.reservation.paginator.ReservationsPaginator
 
-
-class PropertyReservationStream(
-    private val paginator: PropertyReservationsPaginator
-) : PaginatedStream<ReservationData?>() {
+class ReservationsStream(private val paginator: ReservationsPaginator) : PaginatedStream<ReservationData?>() {
     override fun nextItem(): ReservationData? {
         if (isCurrentPageEmpty()) {
             if (!paginator.hasNext()) {
