d91395d7e69499d8fb795028617901984cf1596f
mdwairi <mdwairy@gmail.com>
2024-11-11 16:40:03 +0300

    cleanup plugin code

--- apollo-compiler-plugin/src/main/kotlin/com/expediagroup/sdk/apollo/plugin/StaticBuilderPlugin.kt
+++ apollo-compiler-plugin/src/main/kotlin/com/expediagroup/sdk/apollo/plugin/StaticBuilderPlugin.kt
@@ -10,32 +10,29 @@ import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 
-class StaticBuilderPlugin: ApolloCompilerPlugin {
-    override fun kotlinOutputTransform(): Transform<KotlinOutput>? {
+/**
+ * Custom Apollo Compiler Plugin to enhance generated Kotlin code by adding a static builder method when applicable.
+ */
+class StaticBuilderPlugin : ApolloCompilerPlugin {
+    override fun kotlinOutputTransform(): Transform<KotlinOutput> {
         return object : Transform<KotlinOutput> {
             override fun transform(input: KotlinOutput): KotlinOutput {
                 return KotlinOutput(
                     fileSpecs = input.fileSpecs.map { kotlinFile ->
                         FileSpec.builder(kotlinFile.packageName, kotlinFile.name)
                             .apply {
-                                // Process members of the file
                                 kotlinFile.members.forEach { member ->
                                     when (member) {
                                         is TypeSpec -> {
-                                            // Process TypeSpec (classes, interfaces, etc.)
-                                            val newTypeSpec = processTypeSpec(member)
-                                            addType(newTypeSpec)
+                                            addType(processTypeSpec(member))
                                         }
+
                                         is FunSpec -> {
-                                            // Add functions directly
                                             addFunction(member)
                                         }
+
                                         is PropertySpec -> {
-                                            // Add properties directly
                                             addProperty(member)
-                                        }
-                                        else -> {
-                                            // Handle other possible member types if necessary
                                         }
                                     }
                                 }
@@ -48,95 +45,88 @@ class StaticBuilderPlugin: ApolloCompilerPlugin {
         }
     }
 
-
-    // Helper function to process TypeSpec
+    /**
+     * Process the given TypeSpec to handle the presence of a nested "Builder" class.
+     *
+     * @param typeSpec the TypeSpec to process
+     * @return the processed TypeSpec with necessary modifications for a "Builder" class if applicable
+     */
     private fun processTypeSpec(typeSpec: TypeSpec): TypeSpec {
-        // Only process normal classes
         if (typeSpec.kind != TypeSpec.Kind.CLASS) {
             return typeSpec
         }
 
-        // Check if the class has a nested "Builder" class
         val hasBuilderClass = typeSpec.typeSpecs.any { it.name == "Builder" }
 
-        // If there's no Builder class, return the typeSpec unchanged
         if (!hasBuilderClass) {
             return typeSpec
         }
 
-        // Create a new TypeSpec.Builder for the class
         val newTypeSpecBuilder = typeSpec.toBuilder()
 
-        // Process nested types to find the companion object
         var hasCompanionObject = false
         val newNestedTypes = mutableListOf<TypeSpec>()
-        for (nestedTypeSpec in typeSpec.typeSpecs) {
-            if (nestedTypeSpec.kind == TypeSpec.Kind.OBJECT && (nestedTypeSpec.name == null || nestedTypeSpec.name == "Companion")) {
+
+        typeSpec.typeSpecs.forEach {
+            if (it.kind == TypeSpec.Kind.OBJECT && (it.name == null || it.name == "Companion")) {
                 hasCompanionObject = true
-                // Process the companion object to add the builder method
-                val processedCompanion = processCompanionObject(nestedTypeSpec, typeSpec.name ?: "")
-                newNestedTypes.add(processedCompanion)
+                newNestedTypes.add(processCompanionObject(it, typeSpec.name ?: ""))
             } else {
-                // Add other nested types as is
-                newNestedTypes.add(nestedTypeSpec)
+                newNestedTypes.add(it)
             }
         }
 
-        // If there is no companion object, create one with the builder method
         if (!hasCompanionObject) {
-            val companionObject = createCompanionObjectWithBuilder(typeSpec.name ?: "")
-            newNestedTypes.add(companionObject)
+            newNestedTypes.add(createCompanionObjectWithBuilder(typeSpec.name ?: ""))
         }
 
         // Clear existing nested types and add the new ones
         newTypeSpecBuilder.typeSpecs.clear()
         newTypeSpecBuilder.addTypes(newNestedTypes)
 
-        // Build and return the new TypeSpec
         return newTypeSpecBuilder.build()
     }
 
-    // Helper function to process the companion object
+    /**
+     * Process the companion object to check for the presence of a builder method and add one if it doesn't exist.
+     *
+     * @param companionObject the companion object of type TypeSpec
+     * @param enclosingClassName the name of the enclosing class as a String
+     * @return the modified TypeSpec with the builder method added to the companion object if needed
+     */
     private fun processCompanionObject(companionObject: TypeSpec, enclosingClassName: String): TypeSpec {
-        // Check if the companion object already has a builder method
         val hasBuilderMethod = companionObject.funSpecs.any { it.name == "builder" }
 
         if (hasBuilderMethod) {
-            // Companion object already has a builder method, return it as is
             return companionObject
         }
 
-        // Create a builder from the existing companion object
-        val companionBuilder = companionObject.toBuilder()
-
-        // Create the builder function
-        val jvmStaticAnnotation = AnnotationSpec.builder(JvmStatic::class).build()
-        val builderFunction = FunSpec.builder("builder")
-            .returns(ClassName("", enclosingClassName).nestedClass("Builder"))
-            .addAnnotation(jvmStaticAnnotation)
-            .addStatement("return %T()", ClassName("", enclosingClassName).nestedClass("Builder"))
-            .build()
-
-        // Add the builder function to the companion object
-        companionBuilder.addFunction(builderFunction)
-
-        // Build and return the modified companion object
-        return companionBuilder.build()
-    }
-
-    // Helper function to create a companion object with the builder method
-    private fun createCompanionObjectWithBuilder(enclosingClassName: String): TypeSpec {
-        val jvmStaticAnnotation = AnnotationSpec.builder(JvmStatic::class).build()
-        val builderFunction = FunSpec.builder("builder")
-            .returns(ClassName("", enclosingClassName).nestedClass("Builder"))
-            .addAnnotation(jvmStaticAnnotation)
-            .addStatement("return %T()", ClassName("", enclosingClassName).nestedClass("Builder"))
-            .build()
-
-        val companionObject = TypeSpec.companionObjectBuilder()
-            .addFunction(builderFunction)
-            .build()
-
         return companionObject
+            .toBuilder()
+            .addFunction(constructBuilderStaticMethod(enclosingClassName))
+            .build()
     }
+
+    /**
+     * Helper function to create a companion object with a builder method.
+     *
+     * @param enclosingClassName the name of the enclosing class as a String
+     * @return the TypeSpec representing the companion object with the builder method
+     */
+    private fun createCompanionObjectWithBuilder(enclosingClassName: String) = TypeSpec
+        .companionObjectBuilder()
+        .addFunction(constructBuilderStaticMethod(enclosingClassName))
+        .build()
+
+    /**
+     * Constructs a JvmStatic annotated function spec for a builder method within the specified enclosing class name.
+     *
+     * @param enclosingClassName the name of the enclosing class as a String
+     */
+    private fun constructBuilderStaticMethod(enclosingClassName: String) = FunSpec
+        .builder("builder")
+        .returns(ClassName("", enclosingClassName).nestedClass("Builder"))
+        .addAnnotation(AnnotationSpec.builder(JvmStatic::class).build())
+        .addStatement("return %T()", ClassName("", enclosingClassName).nestedClass("Builder"))
+        .build()
 }
