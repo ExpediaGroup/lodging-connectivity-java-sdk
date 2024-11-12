0bd4ad2cfb3800a4b04f260b2e6a0d69c5816a3a
mdwairi <mdwairy@gmail.com>
2024-11-12 23:32:23 +0300

    chore: add docs strings for sandbox.property package

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/sandbox/property/operation/GetSandboxPropertyOperation.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/sandbox/property/operation/GetSandboxPropertyOperation.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.expediagroup.sdk.lodgingconnectivity.sandbox.property.operation
 
 import com.expediagroup.sdk.graphql.common.GraphQLExecutor
@@ -6,11 +22,31 @@ import com.expediagroup.sdk.graphql.model.response.Response
 import com.expediagroup.sdk.lodgingconnectivity.sandbox.operation.SandboxPropertyQuery
 import com.expediagroup.sdk.lodgingconnectivity.sandbox.operation.fragment.SandboxPropertyData
 
+/**
+ * Represents the response for [SandboxPropertyQuery] GraphQL operation, containing both the processed
+ * sandbox property data and the full raw GraphQL response.
+ *
+ * @param data The processed [SandboxPropertyData] extracted from the raw response, representing
+ * details of the requested sandbox property.
+ * @param rawResponse The raw response from the GraphQL query, including the complete data structure
+ * and any associated errors.
+ */
 data class GetSandboxPropertyResponse(
     override val data: SandboxPropertyData,
     override val rawResponse: RawResponse<SandboxPropertyQuery.Data>,
 ) : Response<SandboxPropertyData, SandboxPropertyQuery.Data>
 
+/**
+ * Executes a [SandboxPropertyQuery] GraphQL query to retrieve details about a specific sandbox property.
+ *
+ * This function uses the provided [GraphQLExecutor] to execute the query and returns a [GetSandboxPropertyResponse]
+ * containing both the targeted sandbox property data and the full raw response.
+ *
+ * @param graphQLExecutor The [GraphQLExecutor] responsible for executing the GraphQL query.
+ * @param propertyId The unique identifier of the sandbox property to retrieve.
+ * @return A [GetSandboxPropertyResponse] containing the requested sandbox property data and the full raw response.
+ * @throws ExpediaGroupServiceException If an error occurs during the query execution.
+ */
 fun getSandboxPropertyOperation(graphQLExecutor: GraphQLExecutor, propertyId: String): GetSandboxPropertyResponse {
     val operation = SandboxPropertyQuery(propertyId)
     val response = graphQLExecutor.execute(operation)
--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/sandbox/property/operation/UpdateSandboxPropertyOperation.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/sandbox/property/operation/UpdateSandboxPropertyOperation.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.expediagroup.sdk.lodgingconnectivity.sandbox.property.operation
 
 import com.expediagroup.sdk.graphql.common.GraphQLExecutor
@@ -7,11 +23,31 @@ import com.expediagroup.sdk.lodgingconnectivity.sandbox.operation.SandboxUpdateP
 import com.expediagroup.sdk.lodgingconnectivity.sandbox.operation.fragment.SandboxPropertyData
 import com.expediagroup.sdk.lodgingconnectivity.sandbox.operation.type.UpdatePropertyInput
 
+/**
+ * Represents the response for [SandboxUpdatePropertyMutation] GraphQL operation, containing both the processed
+ * sandbox property data and the full raw GraphQL response.
+ *
+ * @param data The updated [SandboxPropertyData] extracted from the raw response, representing the details
+ * of the sandbox property after the update.
+ * @param rawResponse The raw response from the GraphQL mutation, including the complete data structure
+ * and any associated errors.
+ */
 data class UpdateSandboxPropertyResponse(
     override val data: SandboxPropertyData,
     override val rawResponse: RawResponse<SandboxUpdatePropertyMutation.Data>,
 ) : Response<SandboxPropertyData, SandboxUpdatePropertyMutation.Data>
 
+/**
+ * Executes [SandboxUpdatePropertyMutation] GraphQL mutation to modify the details of an existing sandbox property.
+ *
+ * This function uses the provided [GraphQLExecutor] to execute the mutation and returns an [UpdateSandboxPropertyResponse]
+ * containing both the updated sandbox property data and the full raw response.
+ *
+ * @param graphQLExecutor The [GraphQLExecutor] responsible for executing the GraphQL mutation.
+ * @param input The [UpdatePropertyInput] containing the details of the property update.
+ * @return An [UpdateSandboxPropertyResponse] containing the updated sandbox property data and the full raw response.
+ * @throws ExpediaGroupServiceException If an error occurs during the mutation execution.
+ */
 fun updateSandboxPropertyOperation(
     graphQLExecutor: GraphQLExecutor,
     input: UpdatePropertyInput
