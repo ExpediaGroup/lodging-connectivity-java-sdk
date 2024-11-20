fb67fe8c61eee0d149c518a0e9b75b190d0b6be0
mdwairi <mdwairy@gmail.com>
2024-11-21 01:36:17 +0300

    feat: add default okhttp client

---
+++ code/src/main/kotlin/com/expediagroup/sdk/core2/okhttp/BaseOkHttpClient.kt
@@ -0,0 +1,44 @@
+package com.expediagroup.sdk.core2.okhttp
+
+import java.time.Duration
+import okhttp3.OkHttpClient
+
+internal object BaseOkHttpClient {
+    @Volatile
+    private var instance: OkHttpClient? = null
+
+    fun getInstance(): OkHttpClient {
+        return instance ?: synchronized(this) {
+            instance ?: OkHttpClient().also { instance = it }
+        }
+    }
+
+    fun getConfiguredInstance(configuration: OkHttpClientConfiguration): OkHttpClient = getInstance()
+        .newBuilder()
+        .apply {
+            configuration.callTimeoutMillis?.let {
+                callTimeout(Duration.ofMillis(it.toLong()))
+            }
+            configuration.connectTimeoutMillis?.let {
+                connectTimeout(Duration.ofMillis(it.toLong()))
+            }
+            configuration.readTimeoutMillis?.let {
+                readTimeout(Duration.ofMillis(it.toLong()))
+            }
+            configuration.writeTimeoutMillis?.let {
+                writeTimeout(Duration.ofMillis(it.toLong()))
+            }
+            configuration.connectionPool?.let {
+                connectionPool(it)
+            }
+            configuration.retryOnConnectionFailure?.let {
+                retryOnConnectionFailure(it)
+            }
+            configuration.interceptors?.forEach {
+                addInterceptor(it)
+            }
+            configuration.networkInterceptors?.forEach {
+                addNetworkInterceptor(it)
+            }
+        }.build()
+}
