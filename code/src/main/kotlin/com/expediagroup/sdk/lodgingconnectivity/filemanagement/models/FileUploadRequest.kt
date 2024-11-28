28dfcd062def37190e43abb56625964535922fd5
Mohammad Dwairi <49045447+Mohammad-Dwairi@users.noreply.github.com>
2024-11-28 18:18:24 +0300

    feat: add injectable http client interfaces (#105)
    Co-authored-by: mohnoor94 <moh.noor94@gmail.com>

--- code/src/main/kotlin/com/expediagroup/sdk/lodgingconnectivity/filemanagement/models/FileUploadRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.expediagroup.sdk.lodgingconnectivity.filemanagement.models
-
-import java.io.File
-import java.io.InputStream
-
-class FileUploadRequest private constructor(val content: Any?) {
-    constructor(file: File) : this(content = file)
-    constructor(inputStream: InputStream) : this(content = inputStream)
-}
