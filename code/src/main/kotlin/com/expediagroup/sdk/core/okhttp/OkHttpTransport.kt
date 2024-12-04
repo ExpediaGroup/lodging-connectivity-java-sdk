f9fad9ae943ffdf603a08014ac8b126a20f4481e
mdwairi <mdwairy@gmail.com>
2024-12-04 18:27:43 +0300

    chore: refactor Request class and add unit tests

--- code/src/main/kotlin/com/expediagroup/sdk/core/okhttp/OkHttpTransport.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/core/okhttp/OkHttpTransport.kt
@@ -58,10 +58,9 @@ class OkHttpTransport(
 
     private fun toOkHttpRequest(sdkRequest: SdkRequest): OkHttpRequest {
         return OkHttpRequestBuilder().apply {
-            url(sdkRequest.url.toUrl())
+            url(sdkRequest.url)
             headers(buildHeaders(sdkRequest.headers))
-            method(sdkRequest.method, createRequestBody(sdkRequest.body))
-            tag(sdkRequest.tags)
+            method(sdkRequest.method.toString(), createRequestBody(sdkRequest.body))
         }.build()
     }
 
