ca1835eec6543abafd8d91e1464abd5267afea15
Mohammad Dwairi <49045447+Mohammad-Dwairi@users.noreply.github.com>
2024-12-05 19:57:37 +0300

    chore: add unit tests for Request classes (#118)

--- code/src/main/kotlin/com/expediagroup/sdk/core/http/Protocol.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/core/http/Protocol.kt
@@ -19,32 +19,25 @@ package com.expediagroup.sdk.core.http;
 /**
  * Enumeration of HTTP protocols.
  */
-enum class Protocol {
-    HTTP_1_0,
-    HTTP_1_1,
-    HTTP_2,
-    H2_PRIOR_KNOWLEDGE,
-    QUIC;
+enum class Protocol(private val protocolString: String) {
+    HTTP_1_0("http/1.0"),
+    HTTP_1_1("http/1.1"),
+    HTTP_2("http/2"),
+    H2_PRIOR_KNOWLEDGE("h2_prior_knowledge"),
+    QUIC("quic");
+
+    override fun toString(): String = protocolString
 
     companion object {
+        private val lookup = Protocol.entries.associateBy { it.protocolString.uppercase() }
+
         /**
          * Parses a protocol string to a [Protocol] enum.
          */
+        @JvmStatic
         fun get(protocol: String): Protocol = when (protocol.uppercase()) {
-            "HTTP/1.0" -> HTTP_1_0
-            "HTTP/1.1" -> HTTP_1_1
             "HTTP/2", "HTTP/2.0" -> HTTP_2
-            "H2_PRIOR_KNOWLEDGE" -> H2_PRIOR_KNOWLEDGE
-            "QUIC" -> QUIC
-            else -> throw IllegalArgumentException("Unexpected protocol: $protocol")
+            else -> lookup[protocol.uppercase()] ?: throw IllegalArgumentException("Unexpected protocol: $protocol")
         }
-    }
-
-    override fun toString(): String = when (this) {
-        HTTP_1_0 -> "http/1.0"
-        HTTP_1_1 -> "http/1.1"
-        HTTP_2 -> "http/2"
-        H2_PRIOR_KNOWLEDGE -> "h2_prior_knowledge"
-        QUIC -> "quic"
     }
 }
