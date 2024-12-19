9d1353999458804b1b0538802e26bfc002a88e20
mdwairi <mdwairy@gmail.com>
2024-12-19 12:22:24 +0300

    chore: revert changes

--- code/src/main/kotlin/com/expediagroup/sdk/graphql/model/exception/NoDataException.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/graphql/model/exception/NoDataException.kt
@@ -16,6 +16,7 @@
 
 package com.expediagroup.sdk.graphql.model.exception
 
+import com.expediagroup.sdk.core.model.exception.service.ExpediaGroupServiceException
 import com.expediagroup.sdk.graphql.model.response.Error
 
 /**
@@ -32,4 +33,4 @@ class NoDataException(
     message: String? = null,
     cause: Throwable? = null,
     val errors: List<Error>,
-) : RuntimeException(message, cause)
+) : ExpediaGroupServiceException(message, cause)
