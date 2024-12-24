5f202b7ef24cc0a6bfcb82e905cd2e626da4e92f
mdwairi <mdwairy@gmail.com>
2024-12-24 16:36:55 +0300

    fix: cleanup ResponseLogger

--- code/src/main/kotlin/com/expediagroup/sdk/core/logging/common/ResponseLogger.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/core/logging/common/ResponseLogger.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.expediagroup.sdk.core.logging.common
 
 import com.expediagroup.sdk.core.http.Response
@@ -6,7 +22,7 @@ import com.expediagroup.sdk.core.logging.common.Constant.DEFAULT_MAX_BODY_SIZE
 import java.nio.charset.Charset
 import okio.Buffer
 
-object ResponseLogger {
+internal object ResponseLogger {
     fun log(
         logger: LoggerDecorator,
         response: Response,
@@ -14,7 +30,7 @@ object ResponseLogger {
         maxBodyLogSize: Long = DEFAULT_MAX_BODY_SIZE
     ) {
         try {
-            val responseBodyString = response.body?.let { it.peekContent(maxBodyLogSize, it.mediaType()?.charset) }
+            val responseBodyString = response.body?.let { it.readLoggableBody(maxBodyLogSize, it.mediaType()?.charset) }
 
             buildString {
                 append("[URL=${response.request.url}, Code=${response.status.code}, Headers=[${response.headers}], Body=[${responseBodyString}]")
@@ -23,11 +39,11 @@ object ResponseLogger {
             }
 
         } catch (e: Exception) {
-            logger.warn("Failed to log response: ${e.message}", e)
+            logger.warn("Failed to log response")
         }
     }
 
-    private fun ResponseBody.peekContent(maxSize: Long, charset: Charset?): String {
+    private fun ResponseBody.readLoggableBody(maxSize: Long, charset: Charset?): String {
         this.mediaType().also {
             if (it === null) {
                 return "Response body of unknown media type cannot be logged"
