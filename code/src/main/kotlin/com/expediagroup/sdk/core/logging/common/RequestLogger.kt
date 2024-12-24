f679d72f402417a2b8875d8b54da645ce1174351
mdwairi <mdwairy@gmail.com>
2024-12-24 16:35:34 +0300

    fix: cleanup RequestLogger

--- code/src/main/kotlin/com/expediagroup/sdk/core/logging/common/RequestLogger.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/core/logging/common/RequestLogger.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.expediagroup.sdk.core.logging.common
 
 import com.expediagroup.sdk.core.http.Request
@@ -7,7 +23,7 @@ import java.io.IOException
 import java.nio.charset.Charset
 import okio.Buffer
 
-object RequestLogger {
+internal object RequestLogger {
     fun log(
         logger: LoggerDecorator,
         request: Request,
@@ -15,7 +31,7 @@ object RequestLogger {
         maxBodyLogSize: Long = DEFAULT_MAX_BODY_SIZE
     ) {
         try {
-            val requestBodyString = request.body?.let { it.peekContent(maxBodyLogSize, it.mediaType()?.charset) }
+            val requestBodyString = request.body?.let { it.readLoggableBody(maxBodyLogSize, it.mediaType()?.charset) }
 
             buildString {
                 append("[URL=${request.url}, Method=${request.method}, Headers=[${request.headers}], Body=[${requestBodyString}]")
@@ -24,12 +40,12 @@ object RequestLogger {
             }
 
         } catch (e: Exception) {
-            logger.warn("Failed to log request: ${e.message}", e)
+            logger.warn("Failed to log request")
         }
     }
 
     @Throws(IOException::class)
-    private fun RequestBody.peekContent(maxSize: Long, charset: Charset?): String {
+    private fun RequestBody.readLoggableBody(maxBodyLogSize: Long, charset: Charset?): String {
         this.mediaType().also {
             if (it === null) {
                 return "Request body of unknown media type cannot be logged"
@@ -40,9 +56,9 @@ object RequestLogger {
             }
         }
 
-        val buffer = Buffer()
-        writeTo(buffer)
-        val bytesToRead = minOf(maxSize, buffer.size)
+        val buffer = Buffer().apply { writeTo(this) }
+        val bytesToRead = minOf(maxBodyLogSize, buffer.size)
+
         return buffer.readString(bytesToRead, charset ?: Charsets.UTF_8)
     }
 }
