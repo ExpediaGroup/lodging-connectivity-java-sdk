f1b369fae7752212eeb5a8252a9dcc8703354377
mdwairi <mdwairy@gmail.com>
2024-12-26 12:44:21 +0300

    feat: add async logging interceptor

--- code/src/main/kotlin/com/expediagroup/sdk/core/http/RequestBody.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/core/http/RequestBody.kt
@@ -137,7 +137,6 @@ abstract class RequestBody {
             }
         }
 
-
         /**
          * Creates a new request body for form data with content type "application/x-www-form-urlencoded".
          *
