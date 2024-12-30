7a0c8996295e6d112a7bb1d80b8d67b5d62b0565
mdwairi <mdwairy@gmail.com>
2024-12-30 20:58:29 +0300

    chore: add BearerAuthenticationAsyncManagerTest

--- core/src/main/kotlin/com/expediagroup/sdk/authentication/bearer/BearerAuthenticationAsyncManager.kt
+++ core/src/main/kotlin/com/expediagroup/sdk/authentication/bearer/BearerAuthenticationAsyncManager.kt
@@ -1,19 +1,41 @@
+/*
+ * Copyright (C) 2024 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.expediagroup.sdk.authentication.bearer
 
 import com.expediagroup.sdk.authentication.common.Credentials
-import com.expediagroup.sdk.pipeline.step.RequestHeadersStep
 import com.expediagroup.sdk.exception.service.ExpediaGroupAuthException
 import com.expediagroup.sdk.http.Request
 import com.expediagroup.sdk.http.Response
+import com.expediagroup.sdk.logging.common.LoggerDecorator
+import com.expediagroup.sdk.pipeline.ExecutionPipeline
+import com.expediagroup.sdk.pipeline.step.RequestHeadersStep
 import com.expediagroup.sdk.pipeline.step.RequestLoggingStep
 import com.expediagroup.sdk.pipeline.step.ResponseLoggingStep
-import com.expediagroup.sdk.logging.common.LoggerDecorator
 import com.expediagroup.sdk.transport.AbstractAsyncRequestExecutor
 import com.expediagroup.sdk.transport.AsyncTransport
-import com.expediagroup.sdk.pipeline.ExecutionPipeline
 import java.util.concurrent.CompletableFuture
 import org.slf4j.LoggerFactory
 
+/**
+ * Manages bearer tokens fetching, parsing, storing, and renewal.
+ * This async implementation uses the injected [AsyncTransport] to make the authentication requests.
+ * Typically, this async implementation should be used with async SDK calls where the [AsyncTransport] is already
+ * configured and used to handle requests.
+ */
 class BearerAuthenticationAsyncManager(
     authUrl: String,
     credentials: Credentials,
@@ -21,7 +43,7 @@ class BearerAuthenticationAsyncManager(
 ) : AbstractBearerAuthenticationManager(authUrl, credentials) {
 
     private val requestExecutor = object : AbstractAsyncRequestExecutor(asyncTransport) {
-        override val executionPipeline: ExecutionPipeline = ExecutionPipeline(
+        override val executionPipeline = ExecutionPipeline(
             requestPipeline = listOf(
                 RequestHeadersStep(),
                 RequestLoggingStep(logger)
@@ -32,33 +54,36 @@ class BearerAuthenticationAsyncManager(
         )
     }
 
+    /**
+     * Initiates authentication to obtain a new bearer token.
+     *
+     * This method sends a request to the authentication server, parses the response, and
+     * stores the token for future use.
+     */
     override fun authenticate() {
-        clearAuthentication()
-            .let {
-                buildAuthenticationRequest()
-            }.let {
-                executeAuthenticationRequest(it).join()
-            }.let {
-                TokenResponse.parse(it)
-            }.also {
-                storeToken(it)
-            }
+        try {
+            clearAuthentication()
+            executeAuthenticationRequest(buildAuthenticationRequest())
+                .thenApply { TokenResponse.parse(it) }
+                .thenAccept { storeToken(it) }
+                .join()
+        } catch (e: Exception) {
+            throw ExpediaGroupAuthException(message = "Authentication Failed", cause = e)
+        }
     }
 
     /**
      * Executes the authentication request and validates the response.
-     *
-     * @param request The [Request] object to be executed.
-     * @return The [Response] received from the server.
-     * @throws ExpediaGroupAuthException If the server responds with an error.
      */
-    private fun executeAuthenticationRequest(request: Request): CompletableFuture<Response> = run {
-        return requestExecutor.execute(request).thenApply {
-            if (!it.isSuccessful) {
-                throw ExpediaGroupAuthException(it.status, "Authentication failed")
+    private fun executeAuthenticationRequest(request: Request): CompletableFuture<Response> {
+        return requestExecutor.execute(request)
+            .thenApply {
+                if (it.isSuccessful) it
+                else throw ExpediaGroupAuthException("Received unsuccessful authentication response: [${it.status}]")
             }
-            it
-        }
+            .exceptionally {
+                throw it
+            }
     }
 
     companion object {
